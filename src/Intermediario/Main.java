1f2c95d48a6b113b06ae07ddb4e5fb966e7ef414
brnlongui <brunorlongui@gmail.com>
2026-01-07 13:24:06 -0400

    dando inicio ao modulo itermediario

---
+++ src/Intermediario/Main.java
@@ -0,0 +1,26 @@
+package Intermediario;
+
+public class Main {
+    public static void main(String[] args) {
+        //Criar Naruto
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da Folha";
+        Naruto.idade = 17;
+
+        //Criar Sassuke
+        Ninja Sassuke = new Ninja();
+        Sassuke.nome = "Sassuke Uchiha";
+        Sassuke.aldeia = "Aldeia da Folha";
+        Sassuke.idade = 18;
+
+        //Criar Sakura
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Aldeia da Folha";
+        Sakura.idade = 18;
+
+        
+    }
+
+}
---
+++ src/Intermediario/Ninja.java
@@ -0,0 +1,8 @@
+package Intermediario;
+
+public class Ninja {
+    String nome = "";
+    String aldeia= "";
+    int idade;
+
+}
